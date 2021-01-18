5e90671c21012216e47eadfe7a3fc91aa8803d58
nasim bahar <nasimbahar78@yahoo.com>
2021-01-18 11:05:33 +0430

    Add project files.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,79 @@
+﻿using Datatable.Models;
+using Elect.Web.DataTable.Models;
+using Elect.Web.DataTable.Models.Request;
+using Elect.Web.DataTable.Models.Response;
+using Elect.Web.DataTable.Processing.Response;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Datatable.Controllers
+{
+    public class HomeController : Controller
+    {
+        private static readonly List<UserModel> DummyUsers = GetDummyUsers();
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Test()
+        {
+            return Ok("Test");
+        }
+
+        /// <summary>
+        ///     Get Users DataTable 
+        /// </summary>
+        /// <returns></returns>
+        [Route("datatable")]
+        [HttpPost]
+        public DataTableActionResult<UserModel> GetDataTable([FromForm] DataTableRequestModel model)
+        {
+            // 1. In Service Layer
+            DataTableResponseModel<UserModel> response = GetDataTableResponse(model);
+
+            // 2. In Controller
+            var result = response.GetDataTableActionResult(model, x => new
+            {
+                IsActive = x.IsActive ? "Yes" : "No" // Transform Data before Response
+            });
+
+            return result;
+        }
+
+        private static DataTableResponseModel<UserModel> GetDataTableResponse(DataTableRequestModel model)
+        {
+            // Queryable Data
+            var query = DummyUsers.AsQueryable();
+
+            // Generate DataTable Response
+            var result = query.GetDataTableResponse(model);
+
+            return result;
+        }
+
+        private static List<UserModel> GetDummyUsers()
+        {
+            var users = new List<UserModel>();
+
+            for (int i = 0; i < 1000; i++)
+            {
+                users.Add(new UserModel
+                {
+                    Id = i + 1,
+                    FullName = $"User {i + 1}",
+                    CreatedTime = DateTimeOffset.Now,
+                    IsActive = i % 2 == 0
+                });
+            }
+
+            return users;
+        }
+    }
+}
---
+++ Models/UserModel.cs
@@ -0,0 +1,24 @@
+﻿using Elect.Web.DataTable.Attributes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Datatable.Models
+{
+    public class UserModel
+    {
+        [DataTable(IsVisible = false, DisplayName = "Id")]
+
+        public int Id { get; set; }
+
+        [DataTable(DisplayName = "Name")]
+        public string FullName { get; set; }
+
+        [DataTable(DisplayName = "Created At")]
+        public DateTimeOffset CreatedTime { get; set; }
+
+        [DataTable(DisplayName = "Actived")]
+        public bool IsActive { get; set; }
+    }
+}
